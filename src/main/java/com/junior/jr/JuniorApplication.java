4da59ad0621471f366829ba5c9529ec7f948043a
FranciscoGJR <franciscoogjr@gmail.com>
2023-04-18 15:06:10 -0300

    [r0.1] - inicializando projeto

---
+++ src/main/java/com/junior/jr/JuniorApplication.java
@@ -0,0 +1,13 @@
+package com.junior.jr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorApplication.class, args);
+	}
+
+}

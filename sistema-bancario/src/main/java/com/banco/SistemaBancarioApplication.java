4dd39ef91dda1d6dfdb051a144f2050dafc3cebb
FranciscoGJR <franciscoogjr@gmail.com>
2023-07-16 16:54:36 -0300

    r[0.1] - Implementando ClienteRepository

--- sistema-bancario/src/main/java/com/banco/SistemaBancarioApplication.java
+++ sistema-bancario/src/main/java/com/banco/SistemaBancarioApplication.java
@@ -1,21 +1,31 @@
 package com.banco;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.banco.model.Cliente;
-import com.banco.model.ContaBancaria;
+import com.banco.repository.ClienteRepository;
 
 @SpringBootApplication
 public class SistemaBancarioApplication {
 
+	public static void divisoria(String titulo) {
+		System.out.println("================== " + titulo + " ==================");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaBancarioApplication.class, args);
 		
-		ContaBancaria conta_a = new ContaBancaria(100, 1, 1000);
-		Cliente cliente_a = new Cliente(1, "Ronaldo", conta_a);
 		
-		System.out.println(cliente_a.toString());
+		divisoria("Teste ClineteRepository.listarTodos()");
+		ClienteRepository clienteRepository = new ClienteRepository();
+		
+		List<Cliente> listaClientes = clienteRepository.listarTodos();
+		
+		for(Cliente cliente : listaClientes)
+			System.out.println(cliente.toString());
 	}
 
 }
---
+++ sistema-bancario/src/main/java/com/banco/repository/ClienteRepository.java
@@ -0,0 +1,27 @@
+package com.banco.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.banco.model.Cliente;
+import com.banco.model.ContaBancaria;
+
+public class ClienteRepository {
+
+	
+	public List<Cliente> listarTodos(){
+		int totalCliente = 5;
+		List<Cliente> listaClientes = new ArrayList<>();
+		
+		for(int i = 0; i < totalCliente; i++) {
+			String nomeCliente = "Carlos" + i;
+			ContaBancaria contaBancariaCliente = new ContaBancaria(i + 100, i, i + 1000);
+			Cliente cliente = new Cliente(i, nomeCliente, contaBancariaCliente);
+		
+			listaClientes.add(cliente);
+		}
+		
+		return listaClientes;
+	}
+	
+}

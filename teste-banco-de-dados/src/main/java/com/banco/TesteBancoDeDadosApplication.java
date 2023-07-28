0a71ba345fe91bca1e6cc83d6cb3f1673445bb78
FranciscoGJR <franciscoogjr@gmail.com>
2023-07-28 11:00:11 -0300

    [Projeto: teste-banco-de-dados] - Adicionando pacotes para arquitetura MVC

---
+++ teste-banco-de-dados/src/main/java/com/banco/TesteBancoDeDadosApplication.java
@@ -0,0 +1,13 @@
+package com.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBancoDeDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBancoDeDadosApplication.class, args);
+	}
+
+}

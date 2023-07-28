5e521e3f5aa7dcb1d65b5d52ea0d4bfe3638fe96
FranciscoGJR <franciscoogjr@gmail.com>
2023-07-28 11:13:24 -0300

    [Projeto: teste-banco-de-dados] - Implementando create(), listAll(), findById() e deleteById() na controller

---
+++ teste-banco-de-dados/src/main/java/com/banco/controller/CursoController.java
@@ -0,0 +1,52 @@
+package com.banco.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banco.model.Curso;
+import com.banco.services.CursoServices;
+
+@RestController
+@RequestMapping("/curso")
+public class CursoController {
+
+	@Autowired 
+	CursoServices cursoServices;
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Curso create(@RequestBody Curso curso) {
+		
+		return cursoServices.create(curso);
+	}
+	
+	
+	@GetMapping()
+	public List<Curso> listAll() {
+		return cursoServices.findAll();
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public Curso findById(@PathVariable("id") Integer id) throws Exception {
+		
+		return cursoServices.findById(id);
+		
+	}
+	
+	
+	@DeleteMapping(value = "/{id}")
+	public void deleteById(@PathVariable("id") Integer id){
+		
+		cursoServices.deleteById(id); 
+	}
+	
+}

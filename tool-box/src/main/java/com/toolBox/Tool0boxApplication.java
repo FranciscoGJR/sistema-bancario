e3261929c71bf01120a9dacfd2bf59876941f713
FranciscoGJR <franciscoogjr@gmail.com>
2023-09-02 18:07:34 -0300

    [Projeto: tool-box], feat[framework]: inicializar projeto Spring

---
+++ tool-box/src/main/java/com/toolBox/Tool0boxApplication.java
@@ -0,0 +1,13 @@
+package com.toolBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tool0boxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tool0boxApplication.class, args);
+	}
+
+}
---
+++ tool-box/src/test/java/com/toolBox/Tool0boxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tool0boxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

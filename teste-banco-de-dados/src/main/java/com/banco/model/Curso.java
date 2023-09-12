c815310e61d3d8e2c2980069264e7730dcc211a3
FranciscoGJR <franciscoogjr@gmail.com>
2023-09-12 11:02:27 -0300

    [Projeto: teste-banco-de-dados], feat(modelagem do banco): implementando atributo de dataDeAtualizacao

--- teste-banco-de-dados/src/main/java/com/banco/model/Curso.java
+++ teste-banco-de-dados/src/main/java/com/banco/model/Curso.java
@@ -3,6 +3,7 @@ package com.banco.model;
 import java.time.LocalDate;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -27,6 +28,7 @@ public class Curso {
 	@Column(name = "data_de_criacao")
 	private LocalDate dataDeCriacao;
 
+	@UpdateTimestamp
 	@Column(name = "data_de_atualizacao")
 	private LocalDate dataDeAtualizacao;
 

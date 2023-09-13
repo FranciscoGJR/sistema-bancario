6531fc686be539e2a59a450387e90b7427d31c87
FranciscoGJR <franciscoogjr@gmail.com>
2023-09-13 17:18:32 -0300

    [Projeto: sistema-bancario], feat(modelagem banco): adicionando relacao entre a tabela Agencia e Cliente

--- sistema-bancario/src/main/java/com/banco/model/Agencia.java
+++ sistema-bancario/src/main/java/com/banco/model/Agencia.java
@@ -1,62 +1,72 @@
 package com.banco.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-
+import jakarta.persistence.OneToMany;
 
 /**
  * Agencia
  */
 @Entity
 public class Agencia {
-	
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer identificador;
-    
-    @ManyToOne
-    private Localizacao localizacao;
 
-    private Integer quantClientes;
-    
-    /**
-     * Construtor que define informações básicas da agencia
-     * @param Localizaca : localizacao da agencia
-     * @param Integer : quantidade de clientes na agencia
-     */
-    public Agencia(Localizacao localizacao, Integer quantClientes) {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer identificador;
+
+	@ManyToOne
+	private Localizacao localizacao;
+	
+	@OneToMany(mappedBy = "agencia")
+	private List<Cliente> clientes = new ArrayList<>();
+
+	private Integer quantClientes;
+
+	/**
+	 * Construtor que define informações básicas da agencia
+	 * 
+	 * @param Localizaca : localizacao da agencia
+	 * @param Integer    : quantidade de clientes na agencia
+	 */
+	public Agencia(Localizacao localizacao, Integer quantClientes) {
 		this.localizacao = localizacao;
 		this.quantClientes = quantClientes;
 	}
-    
-	
+
 	public Integer getIdentificador() {
 		return identificador;
 	}
+
 	public void setIdentificador(Integer identificador) {
 		this.identificador = identificador;
 	}
+
 	public Localizacao getLocalizacao() {
 		return localizacao;
 	}
+
 	public void setLocalizacao(Localizacao localizacao) {
 		this.localizacao = localizacao;
 	}
+
 	public Integer getQuantClientes() {
 		return quantClientes;
 	}
+
 	public void setQuantClientes(Integer quantClientes) {
 		this.quantClientes = quantClientes;
 	}
-    
-	
+
 	@Override
 	public String toString() {
 		return "Agencia [identificador=" + identificador + ", localizacao=" + localizacao + ", quantClientes="
 				+ quantClientes + "]";
 	}
-	
+
 }
--- sistema-bancario/src/main/java/com/banco/model/Cliente.java
+++ sistema-bancario/src/main/java/com/banco/model/Cliente.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 
 /**
@@ -23,6 +25,10 @@ public class Cliente {
     
     @OneToOne
     private ContaBancaria contaBancaria;
+    
+    @ManyToOne
+    @JoinColumn(name = "agencia_id")
+    private Agencia agencia;
     
     private LocalDate nascimento;
 

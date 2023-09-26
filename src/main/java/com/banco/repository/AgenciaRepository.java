68156f78a79dc36ff444528a0206b00fa51b66fc
FranciscoGJR <franciscoogjr@gmail.com>
2023-09-26 17:25:30 -0300

    fix: adicionando notation @Repository

--- src/main/java/com/banco/repository/AgenciaRepository.java
+++ src/main/java/com/banco/repository/AgenciaRepository.java
@@ -1,9 +1,11 @@
 package com.banco.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.banco.model.Agencia;
 
+@Repository
 public interface AgenciaRepository extends JpaRepository<Agencia, Integer>{
 
 }

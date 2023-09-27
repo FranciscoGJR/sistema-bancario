2300688d0592665ce965a237238e422e13c32b02
FranciscoGJR <franciscoogjr@gmail.com>
2023-09-27 17:02:22 -0300

    feature: implementando AgenciaController.buscarPorId()

--- src/main/java/com/banco/controller/AgenciaController.java
+++ src/main/java/com/banco/controller/AgenciaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,10 +25,14 @@ public class AgenciaController {
 	public Agencia criar(@RequestBody Agencia agencia) {
 		return agenciaServices.create(agencia);
 	}
-	
-	@GetMapping
-	public List<Agencia> listarTodos() {
+
+	@GetMapping()
+	public List<Agencia> buscarTodos() {
 		return agenciaServices.findAll();
 	}
 
+	@GetMapping(value = "/{id}")
+	public Agencia buscarPorId(@PathVariable("id") Integer id) throws Exception {
+		return agenciaServices.findById(id);
+	}
 }

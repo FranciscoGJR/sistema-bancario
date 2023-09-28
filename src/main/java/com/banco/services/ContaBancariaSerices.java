96d929b79cb88aa3fe596f1e51503dd69a537dd4
FranciscoGJR <franciscoogjr@gmail.com>
2023-09-28 11:52:01 -0300

    feature: implementando ContaBancariaServices.create()

--- src/main/java/com/banco/services/ContaBancariaSerices.java
+++ src/main/java/com/banco/services/ContaBancariaSerices.java
@@ -3,6 +3,7 @@ package com.banco.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.banco.model.ContaBancaria;
 import com.banco.repository.ContaBancariaRepository;
 
 @Service
@@ -10,6 +11,10 @@ public class ContaBancariaSerices {
 
 	@Autowired
 	private ContaBancariaRepository contaBancariaRepository;
+	
+	public ContaBancaria create(ContaBancaria contaBancaria) {
+		return contaBancariaRepository.save(contaBancaria);
+	}
 	
 	public void deletar(Integer id) {
 		contaBancariaRepository.deleteById(id);

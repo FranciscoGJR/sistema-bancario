3b521a80cd27190600de377d35d1f987e952a77f
FranciscoGJR <franciscoogjr@gmail.com>
2023-09-28 15:12:31 -0300

    feature: implementando ContaBancariaController.buscarTodos()

--- src/main/java/com/banco/controller/ContaBancariaController.java
+++ src/main/java/com/banco/controller/ContaBancariaController.java
@@ -1,8 +1,11 @@
 package com.banco.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +25,11 @@ public class ContaBancariaController {
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ContaBancaria criar(@RequestBody ContaBancaria contaBancaria) {
 		return contaBancariaSerices.create(contaBancaria);
+	}
+
+	@GetMapping
+	public List<ContaBancaria> buscarTodos() {
+		return contaBancariaSerices.findAll();
 	}
 
 	@DeleteMapping(value = "/{id}")

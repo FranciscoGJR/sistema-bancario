777d40b09a9655d6c87af7dea898be1dbdcb09fe
FranciscoGJR <franciscoogjr@gmail.com>
2023-09-28 15:00:17 -0300

    feature: implementando ContaBancariaController.criar()

--- src/main/java/com/banco/controller/ContaBancariaController.java
+++ src/main/java/com/banco/controller/ContaBancariaController.java
@@ -1,23 +1,32 @@
 package com.banco.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.banco.model.ContaBancaria;
 import com.banco.services.ContaBancariaSerices;
 
-@Controller
-@RequestMapping("/conta-bancaria")
+@RestController
+@RequestMapping("/contaBancaria")
 public class ContaBancariaController {
 
 	@Autowired
 	ContaBancariaSerices contaBancariaSerices;
-	
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ContaBancaria criar(@RequestBody ContaBancaria contaBancaria) {
+		return contaBancariaSerices.create(contaBancaria);
+	}
+
 	@DeleteMapping(value = "/{id}")
 	public void deletar(@PathVariable("id") Integer id) {
 		contaBancariaSerices.deletar(id);
 	}
-	
+
 }
